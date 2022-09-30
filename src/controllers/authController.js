74a938517fbcaedadc1de11e8e54ca8effa038fd
BLXCKZ13\crowz <crowz.gac@gmail.com>
2022-09-30 15:10:48 +0700

    auth done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,33 @@
 //database
-const { user } = require("../../models");
+const { User } = require("../../models");
 const { Op } = require("sequelize");
-//validator
-const validator = require("validator");
 //security
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// Generate Token function
-const genToken = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
 
 exports.register = async (req, res, next) => {
+  //request
   try {
+    const { firstName, lastName, email, mobile, username, password } = req.body;
+
+    //security
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //database created
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      mobile,
+      username,
+      password: hashedPassword,
+    });
+    //token created
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+
+    //response
+    res.status(201).json({ token });
   } catch (err) {
     next(err);
   }
@@ -21,6 +35,38 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
+    //request
+    const { username, password } = req.body;
+    //validate
+    if (typeof username !== "string") {
+      throw new Error("username is invalid", 400);
+    }
+    if (typeof password !== "string") {
+      throw new Error("password is invalid", 400);
+    }
+    //find user
+    const pulledUser = await User.findOne({
+      where: { username },
+    });
+    //if user doesnt match database
+    if (!pulledUser) {
+      throw new Error("this username doesnt exist", 400);
+    }
+    //if user password doesnt match database
+    const isPasswordMatch = await bcrypt.compare(password, pulledUser.password);
+    if (!isPasswordMatch) {
+      throw new Error("username or password is invalid", 400);
+    }
+    //token created
+    const token = jwt.sign({ id: pulledUser.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+    //response
+    res.status(200).json({
+      token,
+      firstName: pulledUser.firstName,
+      lastName: pulledUser.lastName,
+    });
   } catch (err) {
     next(err);
   }
@@ -28,6 +74,7 @@ exports.login = async (req, res, next) => {
 
 exports.getUser = async (req, res, next) => {
   try {
+    res.status(200).json({ user: req.user });
   } catch (err) {
     next(err);
   }
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,33 @@
-const { user } = require("../../models");
+const { User } = require("../../models");
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   try {
+    //validate Bearer structor
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith("Bearer")) {
+      throw new Error("unauthenticated");
+    }
+    //validate token
+    const token = authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("unauthenticated");
+    }
+    //verify token with secret_key
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    //find user with token in database
+    const authorizedUser = await User.findOne({
+      where: { id: payload.id },
+      attributes: { exclude: "password" },
+    });
+    //validate user
+    if (!authorizedUser) {
+      throw new Error("unauthenticated");
+    }
+    //pass to another api request
+    req.user = authorizedUser;
+    next();
   } catch (err) {
     next(err);
   }
